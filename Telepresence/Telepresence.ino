768919c24776a502b985a55153157d80fe62844e
Russell Leggett <russ@taggartsoftware.com>
2015-07-07 21:07:24 -0400

    Removed unused sendxulse file, moved Telepresence.ino into folder for easy use by Arduino, and updated Arduino code to get rid of relay, and use the enablers.

--- Telepresence/Telepresence.ino
+++ Telepresence/Telepresence.ino
@@ -5,10 +5,12 @@
 AccelStepper stepper1(AccelStepper::DRIVER, 8, 9);
 AccelStepper stepper2(AccelStepper::DRIVER, 11, 12);
 int powerpin = 7;
+int enable1 = 10;
+int enable2 = 13;
 int spd = 200;    // The current speed in steps/second
 int accel = 150;
-boolean toggle1 = 0;
 volatile int inc = 0;
+
 void setup()
 {
   Serial.begin(9600);
@@ -19,7 +21,16 @@ void setup()
   stepper2.setMaxSpeed(spd);
   stepper2.setAcceleration(accel);
   pinMode(powerpin, OUTPUT);
-  digitalWrite(powerpin, HIGH);
+  pinMode(enable1, OUTPUT);
+  pinMode(enable2, OUTPUT);
+
+  //THIS WAS FOR THE RELAY - MAY BE USED LATER
+  //digitalWrite(powerpin, HIGH);
+
+  //This turns the enablers ON
+  digitalWrite(enable1, LOW);
+  digitalWrite(enable2, LOW);
+
   //set timer1 interrupt at 1Hz (1 sec)
   cli();//stop interrupts
   TCCR1A = 0;// set entire TCCR1A register to 0
@@ -39,70 +50,67 @@ void setup()
 
 void loop()
 {
-  char c;
-  if (Serial.available()) {
-    c = Serial.read();
+  if (Serial.available() > 0)
+  { //if any characters available over serial
+    char c = Serial.read();
 
     if (c == 'f')
     { // forward
       stepper1.move(100000);
       stepper2.move(100000);
-      digitalWrite(powerpin, HIGH);
-      inc = 0;
-      TCNT1 = 0;
-      Serial.write("f\n");
-    }
-
-    if (c == 'b')
+    } else if (c == 'b')
     { // reverse
       stepper1.move(-100000);
       stepper2.move(-100000);
-      digitalWrite(powerpin, HIGH);
-      inc = 0;
-      TCNT1 = 0;
-      Serial.write("b\n");
-    }
-
-    if (c == 'r')
+    } else if (c == 'r')
     { // right
-      stepper1.move(100000);
-      stepper2.move(-100000);
-      digitalWrite(powerpin, HIGH);
-      inc = 0;
-      TCNT1 = 0;
-      Serial.write("r\n");
-    }
-    if (c == 'l')
-    { // left
       stepper1.move(-100000);
       stepper2.move(100000);
-      digitalWrite(powerpin, HIGH);
+    } else if (c == 'l')
+    { // left
+      stepper1.move(100000);
+      stepper2.move(-100000);
+    } else if (c == 's')
+    {
+      stopMotor();
+    } else if (c == 'h') //'h' for who
+    {
+      Serial.write('l'); //'l' is for legs!
+    }
+
+    if (c != 's')
+    {
+      //turn enablers for the motor on and reset to prevent shutdown
+      digitalWrite(enable1, LOW);
+      digitalWrite(enable2, LOW);
       inc = 0;
       TCNT1 = 0;
-      Serial.write("l\n");
-    }
-
-    if (c == 's')
-    { // stop
-      stepper1.stop();
-      stepper2.stop();
-      Serial.write("s\n");
     }
 
 
-  }//serial
+  }
+
+  //always move the motor
   stepper1.run();
   stepper2.run();
 }//loop
 
+void stopMotor() {
+  stepper1.stop();
+  stepper2.stop();
+}
 
 
 ISR(TIMER1_COMPA_vect)
 { //timer1 interrupt 1Hz toggles pin 13
   inc++;
+  if (inc > 25) //25 secs
+  {
+    stopMotor();
+  }
   if (inc > 30)
   {
-    inc = 0;
-    digitalWrite(powerpin, LOW);
+    digitalWrite(enable1, HIGH);
+    digitalWrite(enable2, HIGH);
   }
 }
